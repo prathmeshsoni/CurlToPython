9ee6fa00333070227f6f592a7ba407c62cdc361a
prathmesh <prathmesh.soni51@gmail.com>
2024-04-20 20:12:24 +0530

    Add Html Viewer

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, request, jsonify, render_template, send_from_directory, redirect
+from flask import Flask, request, jsonify, render_template, send_from_directory, redirect, session
 import os
 from converter.curl_to_python import CurlToPython
+import uuid
+
+database = {}
 
 app = Flask(__name__)
+app.secret_key = '9202c558s9c7a4ds42vf96f4f9bf464240acfbb9'
 
 
 # Redirect to a specific URL for 404 errors
@@ -23,8 +27,12 @@ def home():
             return jsonify({'code': f'Error: command should begin with "curl" but instead begins with "{curl}"'})
 
         try:
+            main_data = ''
             if check:
-                main_data = CurlToPython(curl).main_request()
+                try:
+                    main_data = CurlToPython(curl).main_request()
+                except Exception as e:
+                    print('Error:', e)
 
             datas = CurlToPython(curl).get_converted_str()
         except:
@@ -38,6 +46,33 @@ def home():
 
     else:
         return render_template('home.html')
+
+
+@app.route('/html-viewer/', methods=['GET', 'POST'])
+def html_viewer():
+    if request.method == 'POST':
+        html_code = request.get_json().get('html_code', '')
+        data_id = str(uuid.uuid4())
+        
+        database[data_id] = html_code
+        
+        session['data_id'] = data_id
+        
+        items = {'link': '/html-viewer/'}
+        return jsonify(items)
+
+    else:
+        data_id = session.get('data_id')
+        
+        html_code = database.get(data_id, '')
+        
+        if html_code:
+            data = html_code
+        else:
+            data = '<h1>Hello World!!</h1>'
+            
+        items = {'data': data}
+        return render_template('html_viewer.html', items=items)
 
 
 # Response Page
